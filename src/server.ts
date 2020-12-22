17c2f5aeed39b529b271343bf1272db519266e81
Artur Polo Norte <arturcastropolonorte@gmail.com>
2020-12-21 21:03:37 -0300

    :sparkles: continue backend app gobarber again

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,8 @@ import routes from './routes';
 
 const app = express();
 
+app.use(express.json());
 app.use(routes);
-
-app.get('/', (request, response) => {
-  return response.json({
-    message:
-      'this is default design pattern with basic dependecies project in node/ts',
-  });
-});
 
 app.listen(3333, () => {
   console.log('Server started in port 3333');
