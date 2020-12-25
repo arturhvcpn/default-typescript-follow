180889bdb191c741d27de8c0e3ba28615694e941
Artur Polo Norte <arturpolonorte@MacBook-Air-de-Artur.local>
2020-12-25 09:49:50 -0300

    :construction: change type for uuid

--- src/database/migrations/1608668242861-CreateAppointments.ts
+++ src/database/migrations/1608668242861-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1608668242861
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
--- src/database/migrations/1608736277250-CreateUsers.ts
+++ src/database/migrations/1608736277250-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1608736277250 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
