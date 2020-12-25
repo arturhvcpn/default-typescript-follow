fd70fcb019c9124d5dfd0844e35d010f0ae464aa
Artur Polo Norte <arturpolonorte@MacBook-Air-de-Artur.local>
2020-12-25 09:50:36 -0300

    :construction: create migration and updated model Appointments

--- src/models/Appointments.ts
+++ src/models/Appointments.ts
@@ -5,7 +5,11 @@ import {
   PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
 } from 'typeorm';
+
+import User from './User';
 
 @Entity('appointments')
 class Appointment {
@@ -13,7 +17,11 @@ class Appointment {
   id: string;
 
   @Column()
-  provider: string;
+  provider_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User;
 
   @Column('timestamp with time zone')
   date: Date;
