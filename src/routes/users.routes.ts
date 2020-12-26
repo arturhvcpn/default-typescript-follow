61c4ee7f922a002e4cb8d1f21d198815c7098161
Artur Polo Norte <arturpolonorte@MacBook-Air-de-Artur.local>
2020-12-25 22:30:26 -0300

    :truck: added router patch in users.routes

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,17 +1,31 @@
 import { Router } from 'express';
+import multer from 'multer';
+import ensureAuthenticated from '../middleware/ensureAuthenticated';
 import CreateUsersService from '../services/CreateUsersService';
+import uploadConfig from '../config/upload';
+
 const usersRouter = Router();
 
+const upload = multer(uploadConfig);
+
 usersRouter.post('/', async (request, response) => {
-    const { name, email, password } = request.body;
+  const { name, email, password } = request.body;
 
-    const createUsersService = new CreateUsersService();
+  const createUsersService = new CreateUsersService();
 
-    const user = await createUsersService.execute({ name, email, password });
+  const user = await createUsersService.execute({ name, email, password });
 
-    delete user.password;
+  delete user.password;
 
-    return response.json(user);
+  return response.json(user);
 });
 
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    return response.json({ ok: true });
+  },
+);
 export default usersRouter;
