fc6e579e95e0e7af91fcfbc706aeed44da5d5d86
Artur Polo Norte <arturpolonorte@MacBook-Air-de-Artur.local>
2020-12-27 10:41:47 -0300

    :recycle: modified usesrs.routes with  controllers

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,42 +1,22 @@
 import { Router } from 'express';
 import multer from 'multer';
-import { container } from 'tsyringe';
-import ensureAuthenticated from '@modules/users/infra/http/middleware/ensureAuthenticated';
-import CreateUserService from '@modules/users/services/CreateUserService';
-import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 import uploadConfig from '@config/upload';
+import ensureAuthenticated from '@modules/users/infra/http/middleware/ensureAuthenticated';
+import UsersControllers from '@modules/users/infra/http/controllers/UsersControllers';
+import UserAvatarController from '@modules/users/infra/http/controllers/UserAvatarController';
 
 const usersRouter = Router();
+const usersControllers = new UsersControllers();
+const userAvatarController = new UserAvatarController();
 
 const upload = multer(uploadConfig);
 
-usersRouter.post('/', async (request, response) => {
-  const { name, email, password } = request.body;
-
-  const createUserService = container.resolve(CreateUserService);
-
-  const user = await createUserService.execute({ name, email, password });
-
-  delete user.password;
-
-  return response.json(user);
-});
+usersRouter.post('/', usersControllers.create);
 
 usersRouter.patch(
   '/avatar',
   ensureAuthenticated,
   upload.single('avatar'),
-  async (request, response) => {
-    const updateUserAvatarService = container.resolve(UpdateUserAvatarService);
-
-    const user = await updateUserAvatarService.execute({
-      user_id: request.user.id,
-      avatarFilename: request.file.filename,
-    });
-
-    delete user.password;
-
-    return response.json(user);
-  },
+  userAvatarController.update,
 );
 export default usersRouter;
