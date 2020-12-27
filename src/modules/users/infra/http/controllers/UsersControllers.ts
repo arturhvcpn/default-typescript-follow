474e78f5afd671ab96978ac76ab91fd0c3ab4f47
Artur Polo Norte <arturpolonorte@MacBook-Air-de-Artur.local>
2020-12-27 10:40:38 -0300

    :sparkles: create users controllers

---
+++ src/modules/users/infra/http/controllers/UsersControllers.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import CreateUserService from '@modules/users/services/CreateUserService';
+
+export default class UsersControllers {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, email, password } = request.body;
+
+    const createUserService = container.resolve(CreateUserService);
+
+    const user = await createUserService.execute({ name, email, password });
+
+    delete user.password;
+
+    return response.json(user);
+  }
+}
