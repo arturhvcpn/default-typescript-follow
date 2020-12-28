a9d1496c7f12d69556b9d638203af9d6fd17fd53
Artur Polo Norte <arturpolonorte@MacBook-Air-de-Artur.local>
2020-12-28 08:06:09 -0300

    :contruction: working in specs

---
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -0,0 +1,32 @@
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointments';
+import ICreateAppointmentsDTO from '@modules/appointments/dtos/ICreateAppointmentsDTO';
+import { uuid } from 'uuidv4';
+
+class AppointmentsRepository implements IAppointmentsRepository {
+  private appointment: Appointment[] = [];
+
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const findAppointment = this.appointment.find(appointment => {
+      appointment.date === date,
+    });
+
+    return findAppointment;
+  }
+
+  public async create({
+    provider_id,
+    date,
+  }: ICreateAppointmentsDTO): Promise<Appointment> {
+    const appointment = new Appointment();
+
+    Object.assign(appointment, { id: uuid(), provider_id, date });
+
+    this.appointment.push(appointment);
+
+    return appointment;
+  }
+}
+
+export default AppointmentsRepository;
---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,7 @@
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointmentService', () => {
+  it('Should be able to create a new appointment ', () => {});
+
+  it('Should not be able to create a new appointment in same time', () => {});
+});
